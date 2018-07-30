348772cfd2df2c131f5937366244edbd498e40b9
Hasan Karahan <hasan.karahan@blackhan.com>
2018-07-30 16:26:39 +0300

    DD-3162: npm run build

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,12 @@
 "use strict";
+
 function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+    for (var p in m) {
+        if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+    }
 }
 Object.defineProperty(exports, "__esModule", { value: true });
 __export(require("@dizmo/functions-buffered"));
 __export(require("@dizmo/functions-random"));
 exports.default = undefined;
+//# sourceMappingURL=index.js.map
